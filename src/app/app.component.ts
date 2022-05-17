a972d13ad2f9666f9066f22f3b5a031c95753557
web-dave <dave@webdave.de>
2022-05-17 22:51:27 +0200

    foms are awesome!!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'v14';
+  user = { name: '', age: 0 };
+
+  form: FormGroup<{
+    name: FormControl<string>;
+    age: FormControl<number>;
+  }>;
+  constructor(private builder: FormBuilder) {
+    this.form = this.builder.nonNullable.group({
+      name: ['', [], []],
+      age: [0, [], []],
+    });
+
+    this.user = { ...this.user, ...this.form.value };
+  }
 }
